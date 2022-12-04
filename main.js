fcad5fd4c7ac42362c97826a89576486db50e0d3
exprodesigner <mail.exprodesigner@gmail.com>
2022-12-04 21:54:36 +0600

    Updated File
    version 2.2.2

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(() => {
+    $('#slideShow .slick').slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        speed:500,
+        dots:true,
+    });
+   
+});
+
+
+
